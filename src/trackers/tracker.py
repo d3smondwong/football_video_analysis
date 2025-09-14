c7ca29162f5a07f83213b71521767edcc7ef1afb
d3smondwong <d3smondwong@gmail.com>
2025-09-14 12:47:16 +0800

    feat: Add in triangle annotation on ball. Lowered ball detection confidence to 0.1 and raise Player and Ref detection to 0.8

--- src/trackers/tracker.py
+++ src/trackers/tracker.py
@@ -25,7 +25,7 @@ class Tracker:
 
         for i in range(0, len(frames), batch_size):
             batch = frames[i:i + batch_size]
-            detections_batch = self.model.predict(batch, conf=0.6, verbose=False)
+            detections_batch = self.model.predict(batch, conf=0.1, verbose=False)
 
             detections += detections_batch
 
@@ -69,6 +69,26 @@ class Tracker:
                         if class_names[class_id] == 'goalkeeper':
                             detections_supervision.class_id[object_ind] = class_names_inv['player']
 
+                # Filter player/referee detections with confidence > 0.8, keep ball detections as-is
+                player_id = class_names_inv['player']
+                referee_id = class_names_inv['referee']
+                ball_id = class_names_inv['ball']
+                if detections_supervision.class_id is not None and detections_supervision.confidence is not None:
+                    indices = [
+                        i for i, (cid, conf) in enumerate(zip(detections_supervision.class_id, detections_supervision.confidence))
+                        if ((cid == player_id or cid == referee_id) and conf > 0.8) or (cid == ball_id)
+                    ]
+                else:
+                    indices = []
+
+                detections_supervision = sv.Detections(
+                    xyxy=detections_supervision.xyxy[indices] if detections_supervision.xyxy is not None else np.array([]),
+                    confidence=detections_supervision.confidence[indices] if detections_supervision.confidence is not None else np.array([]),
+                    class_id=detections_supervision.class_id[indices] if detections_supervision.class_id is not None else np.array([]),
+                    tracker_id=None if detections_supervision.tracker_id is None else detections_supervision.tracker_id[indices],
+                    data={k: np.array(v)[np.array(indices)] for k, v in detections_supervision.data.items()} if detections_supervision.data else {}
+                )
+
                 # Track the objects across frames
                 detection_with_tracking = self.tracker.update_with_detections(detections=detections_supervision)
 
@@ -89,16 +109,17 @@ class Tracker:
                     if class_id == class_names_inv['referee']:
                         tracking_list["referees"][frame_num][track_id] = {"bbox": bbox}
 
+                    # Commenting out the ball tracking for now as the ball is not tracked very well. Probably due to training data size. Will use the first detected ball in each frame instead
+                    # if class_id == class_names_inv['ball']:
+                    #     tracking_list["ball"][frame_num][track_id] = {"bbox":bbox}
+
+                # As there is only one ball per frame. If multiple balls are detected, take the first one.
+                for frame_detection in detections_supervision:
+                    bbox = frame_detection[0].tolist()
+                    class_id = frame_detection[3]
+
                     if class_id == class_names_inv['ball']:
-                        tracking_list["ball"][frame_num][track_id] = {"bbox":bbox}
-
-                # As there is only one ball per frame. If multiple balls are detected, take the first one. Update: Will use ball tracking to track the ball better
-                # for frame_detection in detections_supervision:
-                #     bbox = frame_detection[0].tolist()
-                #     class_id = frame_detection[3]
-
-                #     if class_id == class_names_inv['ball']:
-                #         tracking_list["ball"][frame_num][1] = {"bbox":bbox}
+                        tracking_list["ball"][frame_num][1] = {"bbox":bbox}
 
             # After processing all frames, save the tracking_list to stub file
             if stub_path is not None:
@@ -117,6 +138,11 @@ class Tracker:
         x_center, y_center = get_center_of_bbox(bbox)
         width = get_bbox_width(bbox)
 
+        ###
+        # Drawing the ellipse to represent the player or referee
+        ###
+
+        # Define ellipse parameters
         axes_length = (int(width), int(0.35*width))
         angle = 0
         # Ellipse is drawn from start_angle to end_angle. Draw -45 - 235 degrees for players to stand out
@@ -128,8 +154,54 @@ class Tracker:
         # Draw the ellipse on the frame
         cv2.ellipse(frame, (x_center, y2), axes_length, angle, start_angle, end_angle, color, thickness, line_type)
 
-        # Put the track ID text near the ellipse
-        # cv2.putText(frame, str(track_id), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        ###
+        # Drawing the rectangle place the player number
+        ###
+
+        # Define rectangle parameters
+        rectangle_width = 40
+        rectangle_height = 20
+
+        rectangle_x1 = x_center - rectangle_width // 2
+        rectangle_y1 = (y2- rectangle_height//2) +15
+        rectangle_x2 = x_center + rectangle_width//2
+        rectangle_y2 = (y2+ rectangle_height//2) +15
+
+        # Define text parameters
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 0.6
+        color_text = (0, 0, 0)  # Black color for text
+        thickness = 2
+
+
+        if track_id is not None:
+            # Draw the rectangle on the frame
+            cv2.rectangle(frame, (int(rectangle_x1), int(rectangle_y1)), (int(rectangle_x2), int(rectangle_y2)), color, cv2.FILLED)
+
+            # Calculate position to center the text in the rectangle
+            x1_text = rectangle_x1 + 12
+            if track_id >= 99:
+                x1_text -= 10
+
+            # Put the track ID text on the rectangle
+            cv2.putText(frame, str(track_id), (int(x1_text), int(rectangle_y1 + 15)), font, font_scale, color_text, thickness)
+
+        return frame
+
+    def _draw_triangle(self, frame, bbox, color):
+        x1, y1, x2, y2 = map(int, bbox)
+        x_center, y_center = get_center_of_bbox(bbox)
+
+        # Define triangle parameters
+        triangle_points = np.array([
+                [x_center,y1],
+                [x_center-10,y1-20],
+                [x_center+10,y1-20],
+            ])
+
+        # Draw the triangle on the frame
+        cv2.drawContours(frame, [triangle_points], 0, color, -1)  # -1 fills the triangle
+        cv2.drawContours(frame, [triangle_points], 0, (0,0,0), 2)  # 2 is the thickness of the outline
 
         return frame
 
@@ -138,12 +210,16 @@ class Tracker:
 
         output_video_frames = []
 
-        # tracks is loaded from the stub file
+        # Check stub file data
         num_stub_frames = len(tracking_list["players"])
         num_video_frames = len(video_frames)
 
-        logger.info(f"Number of frames in stub file: {num_stub_frames}")
-        logger.info(f"Number of frames in video_frames: {num_video_frames}")
+        if num_stub_frames != num_video_frames:
+            logger.info(f"Number of frames in stub file: {num_stub_frames}")
+            logger.info(f"Number of frames in input video: {num_video_frames}")
+
+            logger.warning("Number of frames in stub file does not match number of frames in input video. There might be an error in the stub file or stub file video is different from input video.")
+            return None
 
         for frames_num, frame in enumerate(video_frames):
 
@@ -153,15 +229,20 @@ class Tracker:
             ball_dict = tracking_list["ball"][frames_num]
             referee_dict = tracking_list["referees"][frames_num]
 
-            # Draw players
+            # Draw ellipse to annotate players
             for track_id, player in player_dict.items():
                 color = player.get("team_color",(0,0,255))
                 frame = self._draw_ellipse(frame, player["bbox"],color, track_id)
 
-            # Draw referees
+            # Draw ellipse to annotate referees
             for track_id, referee in referee_dict.items():
                 color = referee.get("team_color",(0,255,255))
                 frame = self._draw_ellipse(frame, referee["bbox"],color, track_id)
+
+            # Draw triangle to annotate ball
+            for track_id, ball in ball_dict.items():
+                color = ball.get("team_color",(0,255,0))
+                frame = self._draw_triangle(frame, ball["bbox"],color)
 
             output_video_frames.append(frame)
 
